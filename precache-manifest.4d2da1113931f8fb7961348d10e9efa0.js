0766f799517d051a2606d2455d981aea4012904b
nazim <nazimaitali88@gmail.com>
2020-10-20 00:23:41 +0100

    Updates

---
+++ precache-manifest.4d2da1113931f8fb7961348d10e9efa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7824311e3e045532e2848e240c6f15",
+    "url": "/react-api-app/index.html"
+  },
+  {
+    "revision": "c54023179666ce366577",
+    "url": "/react-api-app/static/css/2.44309f2b.chunk.css"
+  },
+  {
+    "revision": "c308afcf6c0318e57c63",
+    "url": "/react-api-app/static/css/main.565da788.chunk.css"
+  },
+  {
+    "revision": "c54023179666ce366577",
+    "url": "/react-api-app/static/js/2.2a54bcfa.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/react-api-app/static/js/2.2a54bcfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c308afcf6c0318e57c63",
+    "url": "/react-api-app/static/js/main.be5c6fde.chunk.js"
+  },
+  {
+    "revision": "18a5fdb060ed5758e92e",
+    "url": "/react-api-app/static/js/runtime-main.e65f7d4f.js"
+  }
+]);
